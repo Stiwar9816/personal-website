0144c144f8b9599ce508f5433878f9084c85f5fc
Stiwar9816 <stiwar.asprilla1998@gmail.com>
2021-09-24 00:33:46 -0500

    add title skill

--- src/components/CardSkill.vue
+++ src/components/CardSkill.vue
@@ -1,8 +1,14 @@
 <template>
-  <v-card class="cardSkill my-2 mx-2 justify-center align-center text-center " flat >
+  <v-card class="cardSkill my-2 mx-4 " flat >
+     <v-row class="justify-lg-start cardSkill__row " no-gutters>
       <v-icon class="cardSkill__icon" size="34" >
         {{ icon }}
-      </v-icon>
+      </v-icon> 
+      <v-card-title class="cardSkill__title ">
+        {{ titleSkill }}
+      </v-card-title>
+     </v-row>
+      
   </v-card>
 </template>
 
@@ -14,6 +20,10 @@ export default {
       type: String,
       default: "mdi-language-html5",
     },
+     titleSkill: {
+      type: Object,
+      default: "HTML",
+    },
   },
 };
 </script>
@@ -21,6 +31,21 @@ export default {
 <style lang="scss">
 .cardSkill {
   background-color: var(--c-fou-lighter) !important;
+  &__row {
+    margin-left: calc(var(--gutter) * 5.5) !important;
+     @media only screen and (min-width: 1024px) {
+       margin-left: calc(var(--gutter) * 3.5) !important;
+     }
+  }
+  &__title {
+    font-family: var(--f-secondary);
+    font-size: var(--fs-regular);
+    font-weight: var(--medium);
+    color: var(--c-sec-darker) !important;
+      &:hover{
+      color: var(--c-sec-lighter) !important;
+    }
+  }
   &__icon {
     margin: var(--gutter) 0 var(--gutter) 0;
     color: var(--c-sec-darker) !important;
--- src/components/Skill.vue
+++ src/components/Skill.vue
@@ -9,37 +9,37 @@
           <CardSkill icon="mdi-language-html5"/>          
         </v-col>
          <v-col cols="12" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-vuejs" />          
+          <CardSkill icon="mdi-vuejs" titleSkill="VueJS" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-language-css3" />          
+          <CardSkill icon="mdi-language-css3" titleSkill="CSS3" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-language-javascript" />          
+          <CardSkill icon="mdi-language-javascript" titleSkill="JavaScript" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-react" />          
+          <CardSkill icon="mdi-react" titleSkill="ReactJS" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-wordpress"  />          
+          <CardSkill icon="mdi-wordpress" titleSkill="Wordpress" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-gatsby"  />          
+          <CardSkill icon="mdi-gatsby" titleSkill="GatsbyJS"/>          
         </v-col>
         <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-language-php" />          
+          <CardSkill icon="mdi-language-php" titleSkill="PHP" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-git" />          
+          <CardSkill icon="mdi-git" titleSkill="Git" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-github" />          
+          <CardSkill icon="mdi-github" titleSkill="Github" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-bootstrap" />          
+          <CardSkill icon="mdi-bootstrap" titleSkill="Bootstrap" />          
         </v-col>
          <v-col cols="12" md="3" class="justify-center col-lg-3 col-sm-6 col-md-5">
-          <CardSkill icon="mdi-linux" />          
+          <CardSkill icon="mdi-linux" titleSkill="Linux" />          
         </v-col>
       </v-row>
     </v-container>
